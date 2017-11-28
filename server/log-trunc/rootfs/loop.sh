c2ce9191a4bf0bc4908f2954360f19d4a18bbf0b
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2017-11-28 14:51:14 +0000

    More diagnostics

--- server/log-trunc/rootfs/loop.sh
+++ server/log-trunc/rootfs/loop.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+DB=/drone-data/drone.sqlite
+
 while true; do
     echo "$(date)"
 
-    sqlite3 /drone-data/drone.sqlite "\
+    echo "Total logs: $(sqlite3 $DB 'select count(*) from logs')"
+
+    sqlite3 $DB "\
         update logs set log_data='(log truncated)' where log_id < (select max(log_id) - 20 * 1000 FROM logs); \
-        select 'Affected rows: ' || changes(); \
+        select 'Truncated logs: ' || changes(); \
     "
+
+    echo "Free pages: $(sqlite3 $DB 'pragma freelist_count') of $(sqlite3 $DB 'pragma page_count')"
+    echo ""
 
     sleep 500000
 done
