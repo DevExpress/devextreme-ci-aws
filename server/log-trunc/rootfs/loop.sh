d1c92a6f7f2bb5f95ea44289b6fa847cae0f4370
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2019-03-05 18:12:24 +0000

    Truncate logs in small batches

--- server/log-trunc/rootfs/loop.sh
+++ server/log-trunc/rootfs/loop.sh
@@ -3,18 +3,26 @@
 DB=/drone-data/drone.sqlite
 SURROGATE='[{"pos":0,"out":"(log truncated)"}]'
 
+TRUNC_BATCH_SIZE=5000
+
 while true; do
-    echo "$(date)"
+    TRUNC_END_ID=`sqlite3 $DB 'select max(log_id) - 20 * 1000 from logs'`
 
-    echo "Total logs: $(sqlite3 $DB 'select count(*) from logs')"
+    while [ "$TRUNC_END_ID" -gt 0 ]; do
+        TRUNC_START_ID=$((TRUNC_END_ID-TRUNC_BATCH_SIZE+1))
+        TRUNC_COMMAND="update logs set log_data='$SURROGATE' where log_id between $TRUNC_START_ID and $TRUNC_END_ID"
 
-    sqlite3 $DB "\
-        update logs set log_data='$SURROGATE' where log_id < (select max(log_id) - 20 * 1000 FROM logs); \
-        select 'Truncated logs: ' || changes(); \
-    "
+        echo ""
+        echo "$(date)"
+        echo "$TRUNC_COMMAND"
+        sqlite3 $DB "begin; $TRUNC_COMMAND; commit; select 'Affected: ' || changes();"
 
-    echo "Free pages: $(sqlite3 $DB 'pragma freelist_count') of $(sqlite3 $DB 'pragma page_count')"
+        TRUNC_END_ID=$TRUNC_START_ID
+        sleep 3
+    done
+
     echo ""
+    echo "Free pages: $(sqlite3 $DB 'pragma freelist_count') of $(sqlite3 $DB 'pragma page_count')"
 
     sleep 500000
 done
